cb2268427bca6e6832fcb3c561960f5c13976465
11050 <wstudent898@gmail.com>
2022-01-24 22:03:06 +0300

    Template inheritance

--- market.py
+++ market.py
@@ -1,23 +1,35 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
 
-@app.route('/market')
-def market_page():
-    items = [
-        {'id': 1, 'name': 'T-shirt', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Shoes', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'dress', 'barcode': '231985128446', 'price': 150}
-    ]
-    return render_template('market.html', items=items)
 
-@app.route('/Order')
-def order_page():
-    return render_template('order.html')
+
 @app.route('/jamol')
-def buy():
-    return render_template('jamol.html')
+def buy_page():
+    item2=[
+        {'ID':'Sarvar','Name':'sarvar','Surname':'zokirvich','Price':'2112','University':'amity'},
+        {'ID':'Jamolldin','Name':'jamolldin','Surname':'sodikov','Price':'2121','University':'wiut'},
+        {'ID':'diyora','Name':'Diyorakhone','Surname':'Khodjaeva','Price':'5445','University':'inha'}
+    ]
+    return render_template('jamol.html',item2=item2)
+@app.route('/')
+def name_page():
+    return render_template('order.html')
+@app.route('/home')
+def say_hello():
+    item2=[
+        {'name':'jamolldin','surname':'sodikov','lastname':'sayfudin'},
+        {'name':'diyora,','surname':'khodjaeva','lastname':'sasa'},
+        {'name':'iroda','surname':'karimova','lastname':'shoxsaroy'}
+          ]
+
+    return render_template('home.html',item2=item2)
+
+@app.route('/earn')
+def earn_page():
+    box=[
+        {'currency':'USDT','Cryptocurrency':'bitcoin','Stock':'Nasdaq'},
+        {'currency':'EURO','Cryptocurrency':'Ethereum','Stock':'S&P 500'}
+
+         ]
+    return render_template('earn.html',box=box)
