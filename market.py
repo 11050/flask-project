b6aabef90fc3c7bc92005d2b13157a1127455e65
11050 <wstudent898@gmail.com>
2022-01-24 23:09:26 +0300

    completed all

--- market.py
+++ market.py
@@ -9,18 +9,18 @@ def buy_page():
     item2=[
         {'ID':'Sarvar','Name':'sarvar','Surname':'zokirvich','Price':'2112','University':'amity'},
         {'ID':'Jamolldin','Name':'jamolldin','Surname':'sodikov','Price':'2121','University':'wiut'},
-        {'ID':'diyora','Name':'Diyorakhone','Surname':'Khodjaeva','Price':'5445','University':'inha'}
+        {'ID':'diyora','Name':'Diyorakhone','Surname':'dasbhsahb','Price':'5445','University':'inha'}
     ]
     return render_template('jamol.html',item2=item2)
 @app.route('/')
 def name_page():
-    return render_template('order.html')
+    return render_template('trader.html')
 @app.route('/home')
 def say_hello():
     item2=[
         {'name':'jamolldin','surname':'sodikov','lastname':'sayfudin'},
-        {'name':'diyora,','surname':'khodjaeva','lastname':'sasa'},
-        {'name':'iroda','surname':'karimova','lastname':'shoxsaroy'}
+        {'name':'diyora,','surname':'gozala','lastname':'johsmall'},
+        {'name':'iroda','surname':'lopes','lastname':'johnbig'}
           ]
 
     return render_template('home.html',item2=item2)
@@ -28,8 +28,16 @@ def say_hello():
 @app.route('/earn')
 def earn_page():
     box=[
-        {'currency':'USDT','Cryptocurrency':'bitcoin','Stock':'Nasdaq'},
-        {'currency':'EURO','Cryptocurrency':'Ethereum','Stock':'S&P 500'}
+        {'Currency':'USDT','Cryptocurrency':'bitcoin','Stock':'Nasdaq'},
+        {'Currency':'EURO','Cryptocurrency':'Ethereum','Stock':'S&P 500'}
 
          ]
     return render_template('earn.html',box=box)
+
+@app.route('/trader')
+def trader_page():
+    trader=[
+        {'name':'future','type':'long','type2':'short'},
+        {'name':'trading','type':'long','type2':'short'}
+    ]
+    return render_template('trader.html',trader=trader)
